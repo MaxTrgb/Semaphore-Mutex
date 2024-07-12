5b1068b3bc7e1477fff3644d31b244d716a95fa5
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-07-12 13:11:33 +0300

    Add files via upload

---
+++ Semaphore-Mutex-hw/Task2/Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace Task2
+{
+    internal class Program
+    {
+        private static Mutex mutex = new Mutex(false);
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+
+            Random rand = new Random();
+            int num = rand.Next(1, 5);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            Console.WriteLine("Increment: " + num);
+
+            Console.WriteLine("Press enter");
+            Console.ReadLine();
+
+            Thread thread1 = new Thread(() => increaseValue(nums, num));
+            Thread thread2 = new Thread(() => findMax(nums, num));
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("New array: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void increaseValue(int[] nums, int num)
+        {
+            mutex.WaitOne();
+            Console.WriteLine("Thread 1 started");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] += num;
+            }
+
+            Console.WriteLine("Thread 1 end");
+            mutex.ReleaseMutex();
+        }
+        private static void findMax(int[] nums, int num)
+        {
+            mutex.WaitOne();
+            Console.WriteLine("Thread 2 started");
+
+            Console.WriteLine("Max num: " + nums.Max());
+
+            Console.WriteLine("Thread 2 end");
+            mutex.ReleaseMutex();
+        }
+    }
+}
---
+++ Semaphore-Mutex-hw/Task5/Task5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Task5
+{
+    internal class Program
+    {
+        private static Semaphore semaphore = new Semaphore(3, 3);
+        private static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int threadId = i;
+                Thread thread = new Thread(() => generateNums(threadId));
+                thread.Name = $"thread{threadId}";
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+        private static void generateNums(object id)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} is waiting to enter the semaphore.");
+            semaphore.WaitOne();
+            try
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} has entered the semaphore.");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(random.Next(1, 10));
+                }
+            }
+            finally
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is leaving the semaphore.");
+                semaphore.Release();
+            }
+
+
+        }
+    }
+}
